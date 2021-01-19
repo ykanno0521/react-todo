581303573d2e377295546e7a9a4eb250f424dc69
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-01-19 22:14:22 +0900

    useEffct

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
 // Reactのコンポーネントとわかりやすいようにファイル名を『〜.jsx』とする
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  // useStateで使ったていく変数を設定する
-  const [num, setNum] = useState(0);
-  const contentStyle = {
-    color: "blue",
-    fontSize: "50px"
+
+  const onClickSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
   };
+  // useStateで使ったていく変数を設定する
+
+  // const contentStyle = {
+  //   color: "blue",
+  //   fontSize: "50px"
+  // };
+
+  useEffect(() => {
+    console.log("useEffect");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line
+  }, [num]);
+
   return (
     // 不要な<div>をレンダリングさせないため、<>に置き換える
     <>
@@ -21,11 +42,13 @@ const App = () => {
        */}
       <h1 style={{ color: "red" }}>こんにちは</h1>
       {/* オブジェクトとしてstyleを割り当てる */}
-      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
-      <ColorfulMessage color="pink">元気です。</ColorfulMessage>
-      <p style={contentStyle}>hello</p>
+      {/* <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です。</ColorfulMessage> */}
+      {/* <p style={contentStyle}>hello</p> */}
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <button onClick={onClickSwichShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>( ^ω^ )</p>}
     </>
   );
 };
